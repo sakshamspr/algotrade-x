1fa64edb388606cdd5dbd040cae38b9279a0eadf
sumaymittal <mittalsumay@gmail.com>
2026-04-19 16:29:18 +0530

    new

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -11,14 +11,13 @@ export function AppShell() {
     <div className="min-h-screen bg-background text-foreground">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex flex-col gap-8">
         <Navbar />
-        <div className="flex flex-col gap-4">
-          <Ticker />
-          <MarketNews />
-        </div>
-        <PortfolioTabs />
         <main className="flex-1">
           <Outlet />
         </main>
+        <div className="flex flex-col gap-4">
+          <MarketNews />
+        </div>
+        <PortfolioTabs />
         <Footer />
       </div>
       <Chatbot />
