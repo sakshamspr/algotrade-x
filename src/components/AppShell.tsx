c981cc8f4adcfb9a91259f78d0fb4db5f55a3bcf
sumaymittal <mittalsumay@gmail.com>
2026-04-19 16:29:34 +0530

    feat: add Ticker component to AppShell layout

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -11,6 +11,7 @@ export function AppShell() {
     <div className="min-h-screen bg-background text-foreground">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex flex-col gap-8">
         <Navbar />
+        <Ticker />
         <main className="flex-1">
           <Outlet />
         </main>
